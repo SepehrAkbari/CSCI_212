768dc762a15bc3e5b81bf73f689d41426d200e89
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-02-16 15:50:34 -0600

    Create AreaOfCircle.java

---
+++ Assignments/AreaOfCircle.java
@@ -0,0 +1,22 @@
+/*
+ * @Author: Sepehr Akbari
+ * 02.02.2024
+ * calculates the area of circle
+ */
+package circle;
+import java.util.*;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        scanner.close();
+
+        double area = Math.PI * Math.pow(radius, 2);
+
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
