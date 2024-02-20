1d8c4050c7f228b0d31116998726a5f9ac668eaf
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-02-20 00:54:02 -0600

    Update Primes.java

--- Projects/Primes.java
+++ Projects/Primes.java
@@ -1,7 +1,7 @@
 /*
  * Author @ Sepehr Akbari (Compiler)
  * Feb 28, 2024
- * Title: "Here are - this many - Primes Honey!"
+ * Title: "Here are - this many - Primes; Honey!"
  * Desc: 
     * Write a program called Primes that takes an integer 
     * 'n' from the user and prints the first n prime numbers 
@@ -27,11 +27,14 @@ public class Primes {
         // The amount of prime numbers to be printed
         int n;
 
+        // Printing the title of the program
+        System.out.println("\n Here are - this many - Primes; Honey! \n");
+
         // Getting a valid input from the user (a positive integer & not 0)
         boolean valid = false;
         while (!valid) {
             try {
-                System.out.print("Enter an integer: ");
+                System.out.print("Enter the first n prime numbers to be printed: ");
                 // Getting the input from the user
                 n = sc.nextInt();
                 if (n > 0) {
@@ -67,29 +70,29 @@ public class Primes {
     }
 
     // Function to check if a number is prime or not
-    public static boolean isPrime(int n) {
+    public static boolean isPrime(int x) {
         // Checking if the number is prime or not
         // Steps (2) and (3) are only for optimization purposes
 
         // 1) If the number is less than or equal to 1, it's not prime
-        if (n <= 1) {
+        if (x <= 1) {
             return false; 
         }
 
         // 2) If the number is 2 or 3, it's prime
-        if (n == 2 || n == 3) {
+        if (x == 2 || x == 3) {
             return true; 
         }
 
         // 3) If the number is divisible by 2 or 3, it's not prime
-        if (n % 2 == 0 || n % 3 == 0) {
+        if (x % 2 == 0 || x % 3 == 0) {
             return false; 
         }
 
         // 4) A composite number has a prime divisor less than or equal to sqrt(n)
-        for (int i = 5; i < Math.sqrt(n); i = i + 6) {
+        for (int i = 5; i < Math.sqrt(x); i = i + 6) {
             // If the number is divisible by i or i + 2, it's not prime
-            if (n % i == 0 || n % (i + 2) == 0) {
+            if (x % i == 0 || x % (i + 2) == 0) {
                 return false;
             } 
         }
