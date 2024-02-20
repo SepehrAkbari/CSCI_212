cf93880925f0879dd8fce677c87823b8cff246c6
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-02-20 00:27:45 -0600

    Update Factorize.java

--- Projects/Factorize.java
+++ Projects/Factorize.java
@@ -16,38 +16,53 @@ package Factorize;
 import java.util.*;
 
 public class Factorize {
+   // Scanner object to get the input from the user
    static Scanner sc = new Scanner(System.in);
 
+   // Main function
    public static void main(String[] args) {
+      // Getting a valid input from the user (a positive integer & not 0)
       boolean valid = false;
       while (!valid) {
          try {
             System.out.print("Enter a number: ");
+            // Getting the input from the user
             int n = sc.nextInt();
             if (n > 0) {
+               // Breaking the loop
                valid = true;
+               // Calling the function to print the prime factorization of the number
                factorize(n);
             } else {
                System.out.println("Invalid input. Please enter a positive integer.");
             }
+         // Catching the exception if the import is not an integer
          } catch (InputMismatchException e) {
             System.out.println("Invalid input. Please enter a valid integer.");
+            // Clearing the buffer
             sc.next();
          }
       }
+      // Printing a new line for better readability
       System.out.print("\n");
    }
 
+   // Function to print the prime factorization of the number
    public static void factorize(int n) {
       System.out.print("The prime factorization of " + n + " is: ");
+      // If the number is 1, then the prime factorization is 1 (This is an exception to the rule)
       if (n == 1) {
          System.out.print("1");
+         // Breaking the function
          return;
       }
+      
       for (int i = 2; i <= n; i++) {
+         // If the number is divisible by i, then i is a prime factor
          while (n % i == 0) {
             System.out.print(i);
             n /= i;
+            // If n is not 1, then print a '*' to separate the prime factors
             if (n != 1) {
                System.out.print("*");
             }
