5cacfdaa942a454267f3ffa7872bf653ce735988
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-04-10 10:39:53 -0500

    Create RollRace.java

---
+++ DiceProject/RollRace.java
@@ -0,0 +1,19 @@
+package DiceApp;
+/*
+ * Author @ Sepehr Akbari (Compiler)
+ * File: RollRace Class
+ * April 16, 2024
+ * Title: "Card Game: How I lost my life savings!"
+ * Desc:
+    * Using the same Die class, write a program called RollRace that creates two Die objects, 
+    * and keeps rolling them to see which one comes up three sixes in a row first. 
+    * Continue rolling the dice until one of them wins the race and consider the possibility 
+    * that they might tie. Print the results of each roll, and at the end print the winner and 
+    * total number of rolls that were required.
+ * 
+ * Points: [15 points]
+ */
+
+public class RollRace {
+    
+}
